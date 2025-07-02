13e69247f8bf6a1fd42769482ae86c0d0c475687
Anantharaj.R <anantharaj.r021@gamil.com>
2025-07-02 02:36:49 -0700

    fixed navlinks

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
-const Navbar =  () => {
+const linkCls =
+  'text-lg text-gray-700 hover:text-gray-500 hover:underline transition duration-200';
 
-    return(
-      <nav className = '"fixed top-0 left-0 w-full bg-gray-100 shadow-md z-50'>
-          <div className='max-w-7xl mx-auto px-4 py-4 flex flex-col md:flex-row justify-between items-center space-y-4  '>
+export default function Navbar() {
+  return (
+    <nav className="fixed top-0 left-0 w-full bg-gray-100 shadow-md z-50">
+      <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between px-4 py-4 space-y-4 md:space-y-0">
 
-          <div className='text-5xl text-black font-bold hover:text-gray-500'>
-            <Link to ='/'>Anantharaj</Link>
-         </div>
-         <div className=' flex items-center justify-center space-x-10 py-4 font-bold '>
-             <Link to='/' className='text-lg text-black-700 hover:text-gray-500 hover:underline transition duration-200'>Home</Link>
-             <Link to='/about' className='text-lg text-black-700 hover:text-gray-500 hover:underline transition duration-200'>About</Link>
-             <Link to='/projects' className='text-lg text-black-700 hover:text-gray-500 hover:underline transition duration-200'>Projects</Link>
-             <Link to='/contact' className='text-lg text-black-700 hover:text-gray-500 hover:underline transition duration-200'>Contact</Link>
-         </div>
-         </div>
-      </nav>
-    );
+        <Link to="/" className="text-5xl font-bold text-black hover:text-gray-500">
+          Anantharaj
+        </Link>
+
+        <div className="flex items-center space-x-10">
+          <NavLink end to="/"        className={linkCls}>Home</NavLink>
+          <NavLink     to="about"    className={linkCls}>About</NavLink>
+          <NavLink     to="projects" className={linkCls}>Projects</NavLink>
+          <NavLink     to="contact"  className={linkCls}>Contact</NavLink>
+        </div>
+      </div>
+    </nav>
+  );
 }
-
-export default Navbar;
