4f17ac6c88b3959645a10ce86d119bde73a3af0c
Anantharaj.R <anantharaj.r021@gamil.com>
2025-07-02 01:54:40 -0700

    Fixed homepage routing and updated settings

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -10,10 +10,10 @@ import Contact from './components/Contact';
 function App () {
 
   return (
-   <BrowserRouter>
+   <BrowserRouter  basename="/my-portfolio">
    <Navbar/>
    <Routes>
-     <Route path='/' element={<Home/>} />
+     <Route index element={<Home/>} />
      <Route path='/about' element={<About/>} />
      <Route path='/projects' element={<Projects/>} />
      <Route path='/contact' element={<Contact/>} />
