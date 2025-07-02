ba6fe52a623f5d666f4b5a84b7bc96b7fe661d24
Anantharaj.R <anantharaj.r021@gamil.com>
2025-07-02 08:42:06 -0700

    add my profile

--- my-portfolio/src/components/Home.jsx
+++ my-portfolio/src/components/Home.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React from 'react';
 import { FaLinkedin, FaGithub } from "react-icons/fa";
+import myImage from '../assets/my-profile-png.jpeg';
 
-const Home =() => {
-    return(
-        <section className='px-5 py-5 '>
-            <div className='flex items-center justify-center py-40 '>
-                <div className='flex flex-col items-center p-6 space-y-5 ' >
-                <h1 className='text-3xl space-x-4 '>Hi I'm <span className='text-6xl font-bold text-gray-500 hover:text-gray-400' >Anantharaj</span></h1>
-                
-                <h2 className='text-3xl space-x-4'>i'm Front End Web Developer</h2>
-                <div className="flex space-x-6">
+const Home = () => {
+  return (
+    <section className='px-5 py-10'>
+      <div className='flex flex-col md:flex-row items-center justify-between py-20 max-w-6xl mx-auto'>
+        
+        {/* Left side - Content */}
+        <div className='flex flex-col items-start space-y-6 md:w-1/2'>
+          <h1 className='text-3xl'>
+            Hi I'm <span className='text-6xl font-bold text-gray-500 hover:text-gray-400'>Anantharaj</span>
+          </h1>
+          <h2 className='text-3xl'>I'm a Front End Web Developer</h2>
 
-                <a
+          <div className='flex space-x-6'>
+            <a
               href="https://www.linkedin.com/in/ananth-raj-r-1427a3352/"
               target="_blank"
               rel="noopener noreferrer"
@@ -19,16 +23,26 @@ const Home =() => {
               <FaLinkedin className="text-gray-500 text-4xl hover:text-gray-700" />
             </a>
             <a
-              href="https://github.com/settings/profile"
+              href="https://github.com/anantharajcs"
               target="_blank"
               rel="noopener noreferrer"
             >
               <FaGithub className="text-gray-500 text-4xl hover:text-gray-700" />
             </a>
-            </div>
-                </div>
-            </div>
-        </section>
-    );
-}
+          </div>
+        </div>
+
+        {/* Right side - Image */}
+        <div className='mt-10 md:mt-0 md:w-1/2 flex justify-center'>
+          <img
+            src={myImage}
+            alt="Anantharaj"
+            className='w-72 h-72 object-cover rounded-full shadow-lg'
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
 export default Home;
