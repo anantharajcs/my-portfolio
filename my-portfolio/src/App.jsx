7d994163e3de2fcf70e5e121b2b9e99af51add10
Anantharaj.R <anantharaj.r021@gamil.com>
2025-07-02 03:15:40 -0700

    change app.jsx code index element home

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './components/Home';
@@ -12,13 +12,10 @@ function App() {
       <Navbar />
       <main className="pt-24">
         <Routes>
-          <Route index element={<Home />} /> {/* ✅ This is the fix! */}
+          <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="projects" element={<Projects />} />
           <Route path="contact" element={<Contact />} />
-
-          {/* optional: fallback for wrong URLs */}
-          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
       <Footer />
