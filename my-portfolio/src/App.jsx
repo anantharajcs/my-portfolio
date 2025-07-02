9bd26ea1476d0c2810a2fb18df0c3d20f46cfa4d
Anantharaj.R <anantharaj.r021@gamil.com>
2025-07-02 02:56:29 -0700

    change App.jsx navigate add

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,26 +1,29 @@
-import {Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import  Home  from './components/Home';
+import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 
-
-function App () {
-
+function App() {
   return (
-   <>
-   <Navbar/>
-   <Routes>
-     <Route path="/" element={<Home/>} />
-     <Route path='/about' element={<About/>} />
-     <Route path='/projects' element={<Projects/>} />
-     <Route path='/contact' element={<Contact/>} />
-   </Routes>
-   <Footer/>
-   </> 
+    <>
+      <Navbar />
+      <main className="pt-24">
+        <Routes>
+          <Route index element={<Home />} /> {/* ✅ This is the fix! */}
+          <Route path="about" element={<About />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="contact" element={<Contact />} />
+
+          {/* optional: fallback for wrong URLs */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
   );
 }
 
-export default App; 
+export default App;
